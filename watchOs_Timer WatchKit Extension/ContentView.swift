acc63576f5c861788b15df0f6269a8a8cba566c7
Juwon-Yun <eooll2@daum.net>
2022-08-25 23:28:45 +0900

    feature: watch View

--- watchOs_Timer WatchKit Extension/ContentView.swift
+++ watchOs_Timer WatchKit Extension/ContentView.swift
@@ -8,9 +8,43 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var shownSecondScreen = false
+    @State var timerValue = 1
+    
+    
     var body: some View {
-        Text("Hello, World!")
-            .padding()
+        VStack{
+            
+        Text("Start Tuner for \(timerValue) seconds")
+
+        Picker(selection: $timerValue, label: Text("")) {
+            Text("1").tag(1)
+            Text("5").tag(5)
+            Text("10").tag(10)
+            Text("15").tag(15)
+
+        }
+            NavigationLink(
+                destination: SecondView(
+                    shownSecondScreen: shownSecondScreen,
+                    timerValue: timerValue),
+                isActive: $shownSecondScreen,
+                label: {
+                    Text("Go")
+                    
+                }
+            )
+            
+        }
+    }
+}
+
+struct SecondView : View{
+    @State var shownSecondScreen:Bool
+    @State var timerValue = 1
+    
+    var body: some View{
+        Text("second View")
     }
 }
 
