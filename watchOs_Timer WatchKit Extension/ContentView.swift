ccc86262d3453e3ec8f69ac0888f42e9f0b3fa7c
Juwon-Yun <eooll2@daum.net>
2022-08-25 23:58:47 +0900

    feature: spacer

--- watchOs_Timer WatchKit Extension/ContentView.swift
+++ watchOs_Timer WatchKit Extension/ContentView.swift
@@ -10,31 +10,36 @@ import SwiftUI
 struct ContentView: View {
     @State var shownSecondScreen = false
     @State var timerValue = 1
+    @ScaledMetric var borderWidth: CGFloat = 20
     
     
     var body: some View {
         VStack{
+            Spacer()
+            Text("Start Timer for \(timerValue) seconds")
+                .font(.system(size: 16))
             
-        Text("Start Tuner for \(timerValue) seconds")
-
-        Picker(selection: $timerValue, label: Text("")) {
-            Text("1").tag(1)
-            Text("5").tag(5)
-            Text("10").tag(10)
-            Text("15").tag(15)
-            Text("20").tag(20)
-            Text("25").tag(25)
-            Text("30").tag(30)
-
-        }
+            Picker(selection: $timerValue, label: Text("")) {
+                Text("1").tag(1)
+                Text("5").tag(5)
+                Text("10").tag(10)
+                Text("15").tag(15)
+                Text("20").tag(20)
+                Text("25").tag(25)
+                Text("30").tag(30)
+                
+            }
+            .border(Color.black, width: borderWidth)
+            
+            
             NavigationLink(
                 destination: SecondView(
-                    shownSecondScreen: shownSecondScreen,
+                    shownSecondScreen: $shownSecondScreen,
                     timerValue: timerValue),
                 isActive: $shownSecondScreen,
                 label: {
                     Text("Go")
-                    
+                        .font(.system(size: 14))
                 }
             )
             
@@ -43,15 +48,16 @@ struct ContentView: View {
 }
 
 struct SecondView : View{
-    @State var shownSecondScreen:Bool
-    @State var timerValue = 1
+    @Binding var shownSecondScreen:Bool
+    @State var timerValue:Int
     
     var body: some View{
         VStack{
-        if timerValue > 0 {
-           
+            Spacer()
+            if timerValue > 0 {
+                
                 Text("Time remaining")
-                    .font(.system(size: 14))
+                    .font(.system(size: 18))
                 Text("\(timerValue)")
                     .font(.system(size: 40))
                     .onAppear(){
@@ -62,24 +68,26 @@ struct SecondView : View{
                         }
                     }
                 Text("seconds")
-                    .font(.system(size: 14))
+                    .font(.system(size: 18))
+                Spacer()
                 Button(action: {
-                        self.shownSecondScreen = false
-                    }, label: {
-                        Text("Cancel")
-                            .foregroundColor(.red)
-                        
-                    }
+                    self.shownSecondScreen = false
+                }, label: {
+                    Text("Cancel")
+                        .foregroundColor(.red)
+                    
+                }
                 )
-            
-        } else {
-            Button(action: {
-                self.shownSecondScreen = false
-            }, label: {
-                Text("Cancel")
-                    .foregroundColor(.green)
-            })
-        }
+                
+            } else {
+                Spacer()
+                Button(action: {
+                    self.shownSecondScreen = false
+                }, label: {
+                    Text("Done")
+                        .foregroundColor(.green)
+                })
+            }
         }
     }
 }
